e3f541287868e06717987644e8f7f7b54670e331
Lautaro <lautarovanh@gmail.com>
2026-06-18 12:01:25 -0300

    Se registro el interceptor con @Configuration

--- src/main/java/com/lautaro/curso/springboot/calendar/interceptor/springboot_horario/interceptors/CalendarInterceptor.java
+++ src/main/java/com/lautaro/curso/springboot/calendar/interceptor/springboot_horario/interceptors/CalendarInterceptor.java
@@ -15,13 +15,15 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import tools.jackson.databind.ObjectMapper;
 
-@Component
+@Component("calendarInterceptor")
 public class CalendarInterceptor implements HandlerInterceptor {
     
     @Value("${config.calendar.open}")
     private Integer open;
+
     @Value("${config.calendar.close}")
     private Integer close;
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
             throws Exception {
@@ -33,16 +35,17 @@ public class CalendarInterceptor implements HandlerInterceptor {
         if (hour >= open && hour < close) {
             response.setStatus(HttpServletResponse.SC_FORBIDDEN);
             StringBuilder message = new StringBuilder();
-            message.append("Bienvenido al sistema, el horario de atención es de ")
-            .append(open)
-            .append(" a ")
-            .append(close)
-            .append(" horas, por favor, respete el horario de atención");
+            message.append("El horario de atención es de ");
+            message.append(open);
+            message.append(" a ");
+            message.append(close);
+            message.append(" horas, por favor, respete el horario de atención");
             request.setAttribute("message", message.toString());
             return true;
         }
+
         ObjectMapper mapper = new ObjectMapper();
-        Map<String, Object> data = new HashMap<>();
+        Map<String, String> data = new HashMap<>();
         StringBuilder message = new StringBuilder();
         message.append("El sistema se encuentra cerrado, el horario de atención es de ");
         message.append(open);
@@ -50,10 +53,11 @@ public class CalendarInterceptor implements HandlerInterceptor {
         message.append(close);
         message.append(" horas, gracias por su comprensión");
         data.put("message", message.toString());
-        data.put("date", new Date());
+        data.put("date", new Date().toString());
         response.setContentType("application/json");
         response.setStatus(401);
         response.getWriter().write(mapper.writeValueAsString(data));
+
         return false;
     }
 
